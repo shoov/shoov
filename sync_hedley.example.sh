1ef19705b0cd2595f5642d2bdf50dc9d0e928fa5
Amitaibu <amitai@gizra.com>
2015-02-18 15:53:30 +0200

    First commit

---
+++ sync_hedley.example.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################################################################################
+#
+# This script will sync back to the original generator.
+#
+#
+################################################################################
+
+
+GENERATOR_FOLDER="/var/www/generator-hedley"
+
+# Client
+rsync -avz --exclude-from 'sync_hedley_exclude.txt' ./client $GENERATOR_FOLDER/app/templates
+
+# Behat
+rsync -avz --exclude-from 'sync_hedley_exclude.txt' ./behat $GENERATOR_FOLDER/app/templates
+
+# PhantomCSS
+rsync -avz --exclude-from 'sync_hedley_exclude.txt' ./phantomcss $GENERATOR_FOLDER/app/templates
+
+# Drupal - we make sure to hardcode the copy to "boom".
+rsync -avz --exclude-from 'sync_hedley_exclude.txt' ./boom $GENERATOR_FOLDER/app/templates
+
+
+
