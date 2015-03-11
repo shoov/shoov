68730c7f5b261846503e7e4d997da8cb47f3610a
Amitaibu <amitai@gizra.com>
2015-03-11 14:22:36 +0200

    Fix grunt

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -421,7 +421,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'https://github.com/Gizra/boom.git',
+          remote: 'git@github.com:Gizra/boom.git',
           branch: 'gh-pages',
           commit: true,
           push: true
