aa6e3d40e443642c4f46dfbb756c8c20a9b71971
Amitaibu <amitai@gizra.com>
2015-03-29 15:49:41 +0300

    Built client from commit 28ba0c4 on branch master

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'https://dev-shoov.pantheon.io',debugUiRouter:false})
+.constant('Config', {backend:'https://dev-shoov.pantheon.io',debugUiRouter:false,githubClientId:'007e6cb379e60bede712'})
 
 ;
