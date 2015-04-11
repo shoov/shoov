0c4b24a50438b97c154159555e76b2bc4598e62f
Amitai Burstein <amitai@gizra.com>
2015-04-11 21:04:27 +0300

    Add restful

---
+++ shoov/modules/custom/shoov_restful/plugins/restful/message/ci_build_items/1.0/ShoovCiBuildItemsResource.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains ShoovCiBuildItemsResource.
+ */
+
+class ShoovCiBuildItemsResource extends \RestfulEntityBase {
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['start_timestamp'] = array(
+      'property' => 'field_ci_build_timestamp',
+    );
+
+    $public_fields['status'] = array(
+      'property' => 'field_ci_build_status',
+    );
+
+    $public_fields['build'] = array(
+      'property' => 'field_ci_build',
+      'resource' => array(
+        'ci_build' => array(
+          'name' => 'ci_builds',
+          'full_view' => FALSE,
+        ),
+      ),
+    );
+
+    return $public_fields;
+  }
+}
