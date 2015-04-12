acbbcfcfeec59ec58142239f1ef371ccdd423dc0
Amitaibu <amitai@gizra.com>
2015-04-12 09:47:08 +0300

    Remove field_ssh_private_key

--- shoov/modules/custom/shoov_restful/plugins/restful/node/repositories/1.0/ShoovRepositoriesResource.class.php
+++ shoov/modules/custom/shoov_restful/plugins/restful/node/repositories/1.0/ShoovRepositoriesResource.class.php
@@ -14,14 +14,6 @@ class ShoovRepositoriesResource extends \ShoovEntityBaseNode {
   public function publicFieldsInfo() {
     $public_fields = parent::publicFieldsInfo();
 
-    // @todo: Add encryption
-    $public_fields['ssh_private_key'] = array(
-      'property' => 'field_ssh_private_key',
-      'access_callbacks' => array(
-        array($this, 'accessSshPrivateKey'),
-      ),
-    );
-
     unset($public_fields['updated']);
 
     return $public_fields;
