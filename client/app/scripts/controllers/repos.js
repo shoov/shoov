4b9f88ac11ad58f23c85acda114d1f391933dfda
Amitai Burstein <amitai@gizra.com>
2015-04-20 23:26:40 +0300

    fix

--- client/app/scripts/controllers/repos.js
+++ client/app/scripts/controllers/repos.js
@@ -12,4 +12,41 @@ angular.module('clientApp')
 
     $scope.repos = repos;
 
+
+    angular.forEach($scope.repos, function(value, key) {
+      $scope.repos[key].selected = false;
+    });
+
+    // Selected repos.
+    $scope.selection = [];
+
+    // Helper method to get selected repos.
+    $scope.selectedRepos = function selectedRepos() {
+      return filterFilter($scope.repos, { selected: true });
+    };
+
+    // Watch repos for changes.
+    $scope.$watch('repos|filter:{selected:true}', function(repos) {
+      $scope.selection = repos.map(function(repo, key) {
+        return key;
+      });
+    }, true);
+
+    /**
+     * Enable a repo and create a build.
+     *
+     * @param id
+     *   The screenshot ID.
+     */
+    $scope.enableBuild = function(id) {
+      // We don't wait for the delete too actually happen on the server side,
+      // but immediately hide the image.
+      // @todo: Use RamdaJs
+      angular.forEach($scope.repos, function(value, key) {
+        if (value.id == id) {
+          $scope.repos.splice(key, 1);
+        }
+      });
+    };
+
   });
