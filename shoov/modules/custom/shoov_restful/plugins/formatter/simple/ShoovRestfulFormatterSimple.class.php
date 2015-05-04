7405bda9bd6fe9fbb70774ca7353945b4859b10a
Amitai Burstein <amitai@gizra.com>
2015-05-04 13:13:11 +0300

    Easier config of ~/.shoov.json

---
+++ shoov/modules/custom/shoov_restful/plugins/formatter/simple/ShoovRestfulFormatterSimple.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains ShoovRestfulFormatterSimple.
+ */
+
+class ShoovRestfulFormatterSimple extends \RestfulFormatterBase implements \RestfulFormatterInterface {
+
+  /**
+   * Content Type
+   *
+   * @var string
+   */
+  protected $contentType = 'application/json; charset=utf-8';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepare(array $data) {
+    // If we're returning an error then set the content type to
+    // 'application/problem+json; charset=utf-8'.
+    if (!empty($data['status']) && floor($data['status'] / 100) != 2) {
+      $this->contentType = 'application/problem+json; charset=utf-8';
+      return $data;
+    }
+
+    return $data[0];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(array $structured_data) {
+    return drupal_json_encode($structured_data);
+  }
+}
+
