94d6807988dbfcfd626d900add53804b15d72738
Amitai Burstein <amitai@gizra.com>
2015-05-10 10:39:49 +0200

    Add CI-incident resource

---
+++ shoov/modules/custom/shoov_restful/plugins/restful/node/ci_incidents/1.0/ShoovCiIncidentsResource.class.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains ShoovCiIncidentsResource.
+ */
+
+class ShoovCiIncidentsResource extends \ShoovEntityBaseNode {
+
+
+  /**
+   * Overrides \RestfulEntityBaseNode::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['failing_build'] = array(
+      'property' => 'field_failing_build',
+      'resource' => array(
+        'ci_build' => array(
+          'name' => 'ci-build-items',
+          'full_view' => FALSE,
+        ),
+      ),
+    );
+
+    $public_fields['fixed_build'] = array(
+      'property' => 'field_fixed_build',
+      'resource' => array(
+        'ci_build' => array(
+          'name' => 'ci-build-items',
+          'full_view' => FALSE,
+        ),
+      ),
+    );
+
+    $public_fields['error'] = array(
+      'property' => 'field_ci_build_error',
+    );
+
+    $public_fields['ci_build'] = array(
+      'property' => 'field_ci_build',
+      'resource' => array(
+        'ci_build' => array(
+          'name' => 'ci-builds',
+          'full_view' => FALSE,
+        ),
+      ),
+    );
+
+    $public_fields['repository'] = array(
+      'property' => 'og_repo',
+      'resource' => array(
+        'repository' => array(
+          'name' => 'repositories',
+          'full_view' => FALSE,
+        ),
+      ),
+    );
+
+    return $public_fields;
+  }
+}
