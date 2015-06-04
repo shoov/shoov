65b1052040baf5934ef84ac412881c07af24d08b
Ilya Manyahin <ilya@gizra.com>
2015-06-04 12:28:52 +0300

    Make messages import work

--- shoov/modules/custom/shoov_migrate/handlers/entities/ShoovCiBuildMessagesMigrate.php
+++ shoov/modules/custom/shoov_migrate/handlers/entities/ShoovCiBuildMessagesMigrate.php
@@ -25,14 +25,40 @@ class ShoovCiBuildMessagesMigrate extends ShoovMigrateMessage {
 
   public function __construct() {
     parent::__construct();
-    $this->description = t('Import Ci Items from a CSV file.');
+    $this->description = t('Import Ci Build messages from a CSV file.');
 
-    $this->addFieldMapping('field_ci_build', '_ci_build')
+    // Map User.
+    $this
+      ->addFieldMapping('uid', '_ci_build')
+      ->sourceMigration('ShoovCiBuildMigrate')
+      ->callbacks(array($this, 'getUidFromCiBuild'));
+
+    // Map CI Build.
+    $this
+      ->addFieldMapping('field_ci_build', '_ci_build')
       ->sourceMigration('ShoovCiBuildMigrate');
 
+    // Map Status.
     $this->addFieldMapping('field_ci_build_status', '_status');
 
+    // Map Start Time.
     $this->addFieldMapping('field_ci_build_timestamp', '_start_time');
 
   }
+
+  /**
+   * Implements Callback function.
+   *
+   * Return the author ID of the specific CI Build.
+   *
+   * @param $ci_build_id
+   *  Node ID of repository.
+   * @return mixed
+   *  Author ID of CI Build.
+   */
+  protected function getUidFromCiBuild($ci_build_id) {
+      $ci_build_node = node_load($ci_build_id['destid1']);
+      return $ci_build_node->uid;
+  }
+
 }
