231d0018550085f412a1e32e46640cd712369a0a
Ilya Manyahin <ilya@gizra.com>
2015-06-09 14:38:32 +0300

    Fix defects

--- shoov/modules/custom/shoov_restful/plugins/restful/encrypt/ShoovEncryptResource.class.php
+++ shoov/modules/custom/shoov_restful/plugins/restful/encrypt/ShoovEncryptResource.class.php
@@ -21,8 +21,6 @@ class ShoovEncryptResource extends \RestfulEntityBase {
    * Overrides \RestfulBase::publicFieldsInfo().
    */
   public function publicFieldsInfo() {
-    $public_fields = parent::publicFieldsInfo();
-
     $public_fields['encrypt'] = array(
       'callback' => array($this, 'getEncrypt'),
     );
@@ -31,33 +29,48 @@ class ShoovEncryptResource extends \RestfulEntityBase {
   }
 
   /**
+   * Function it is a simple proxy between client request to encode a params and
+   * nodejs server who have encrypting tool to do that.
+   *
    * @param $value
+   *   CI Build object in wrapper.
    * @return string
+   *   Encrypted string.
    */
   protected function getEncrypt($value) {
-    $privateKey = $value->field_private_key->value();
-
-    if (empty($privateKey)) {
-      throw new Exception('CI Build should contain valid Private Key');
-    }
+    $private_key = $value->field_private_key->value();
 
     $request = $this->getRequest();
-    $keyToConvert = array_key_exists('key', $request) ? $request['key'] : NULL;
-    $valueToConvert = array_key_exists('value', $request) ? $request['value'] : NULL;
 
-    if (empty($keyToConvert) || empty($valueToConvert)) {
-      throw new Exception('Request should contain key and value keys.');
+    if (empty($request['key'])) {
+      throw new \RestfulBadRequestException("Request should contain a key.");
     }
 
-    $nodeServerUrl = variable_get('shoov_ui_build_pr_server', 'http://localhost:3000');
-    $url = $nodeServerUrl . '/encrypt';
-    $options = [
+    if (empty($request['value'])) {
+      throw new \RestfulBadRequestException("Request should contain a value.");
+    }
+
+    $node_js_url = variable_get('shoov_ui_build_pr_server', 'http://localhost:3000');
+    $url = $node_js_url . '/encrypt';
+
+    $data = array(
+      'privateKey' => $private_key,
+      'keyToConvert' => $request['key'],
+      'valueToConvert' => $request['value'],
+    );
+
+    $options = array(
       'method' => 'POST',
-      'data' => "privateKey={$privateKey}&keyToConvert={$keyToConvert}&valueToConvert={$valueToConvert}",
+      'data' => http_build_query($data),
       'headers' => array('Content-Type' => 'application/x-www-form-urlencoded'),
-    ];
+    );
 
     $response = drupal_http_request($url, $options);
+
+    if ($response->code != 200) {
+      throw new \RestfulServerConfigurationException("Can't connect to NodeJS server.");
+    }
+
     $json = json_decode($response->data);
 
     return $json->encrypt;
