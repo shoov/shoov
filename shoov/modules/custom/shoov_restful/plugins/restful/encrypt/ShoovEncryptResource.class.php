122cd1f532c64fbfdf83d2ff3eab0df2c53e537e
Amitaibu <amitai@gizra.com>
2015-06-09 10:33:59 +0300

    Add encrypt resource

---
+++ shoov/modules/custom/shoov_restful/plugins/restful/encrypt/ShoovEncryptResource.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains ShoovEncryptResource.
+ */
+
+class ShoovEncryptResource extends \RestfulBase {
+
+  /**
+   * Overrides \RestfulEntityBase::controllers.
+   */
+  protected $controllers = array(
+    '' => array(
+      \RestfulInterface::GET => 'getEncrypt',
+    ),
+  );
+
+
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['encrypt'] = array(
+      'callback' => array($this, 'getEncrypt'),
+    );
+
+    return $public_fields;
+  }
+
+  /**
+   * @param $value
+   * @return string
+   */
+  protected function getEncrypt($value) {
+    $request = $this->getRequest();
+
+    $server_url = variable_get('shoov_ui_build_pr_server', 'http://localhost:3000');
+
+    drupal_http_request()
+
+  }
+
+}
