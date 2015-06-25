8cddf08657c5c2a59e2d876e76641c4c1913eef6
Helena Eksler <helena@gizra.com>
2015-06-25 15:15:09 +0300

    Use backend path.

--- client/app/scripts/controllers/encrypt.js
+++ client/app/scripts/controllers/encrypt.js
@@ -8,12 +8,11 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('EncryptCtrl', function ($scope, build, $location, $http) {
+  .controller('EncryptCtrl', function ($scope, build, $http, Config) {
     $scope.build = build[0];
 
     $scope.getKey = function(data) {
-      console.log(data);
-      var url =  "http://localhost/shoov/www/api/v1.0/encrypt/" + $scope.build.id ;
+      var url =  Config.backend + "/api/v1.0/encrypt/" + $scope.build.id ;
       var value = {
         "key": data.keyName,
         "value": data.keyValue
