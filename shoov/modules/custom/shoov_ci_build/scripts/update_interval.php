1f57775447dccdab425220946d83c5634540db04
Ilya Manyahin <ilya@gizra.com>
2015-07-15 14:18:52 +0300

    create script

---
+++ shoov/modules/custom/shoov_ci_build/scripts/update_interval.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This script related to issue #116.
+ * It is update all old CI builds items to new interval value.
+ */
+
+drush_print("Start updating interval for next CI builds:");
+
+$query = new EntityFieldQuery();
+$results = $query
+  ->entityCondition('entity_type', 'node')
+  ->entityCondition('bundle', 'ci_build')
+  ->execute();
+
+foreach ($results['node'] as $node) {
+  $wrapper = entity_metadata_wrapper('message', $node->nid);
+
+  if (!$wrapper->field_ci_build_interval->value()) {
+    // Set default value 3 Minutes.
+    $wrapper->field_ci_build_interval->set('180');
+  }
+}
