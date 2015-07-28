1da78f34417e6eca5aac92e7c16152f845881231
nader77 <nader@gizra.com>
2015-07-28 15:59:54 +0300

    Add In progress status & CI Build may be disabled

--- client/app/scripts/controllers/repo.js
+++ client/app/scripts/controllers/repo.js
@@ -13,13 +13,21 @@ angular.module('clientApp')
     $scope.ciBuildItems = [];
     $scope.incidents = incidents;
 
-    // Separate the queue item from the list.
+    // List of build statuses.
+    $scope.buildStatuses = {
+      'queue': 'Queue',
+      'in_progress': 'In progress',
+      'error': 'Error',
+      'done': 'Done'
+    };
+
+    // Separate the queue or in progress item from the history list.
     angular.forEach(ciBuildItems, function(item) {
-      if (item.status != 'queue') {
+      if (item.status != 'queue' && item.status != 'in_progress') {
         $scope.ciBuildItems.push(item);
       }
       else {
-        $scope.ciBuildQueueItem = item;
+        $scope.ciBuildItemQueueOrInProgress = item;
       }
     });
 
@@ -34,9 +42,9 @@ angular.module('clientApp')
     channel.bind('ci_build_new', function(data) {
       // Put the current queue item in the beginning of the list because it
       // needs it to be updated in the "ci_build_update" bind.
-      $scope.ciBuildItems.unshift($scope.ciBuildQueueItem);
+      $scope.ciBuildItems.unshift($scope.ciBuildItemQueueOrInProgress);
       // Set the new item as the queue item.
-      $scope.ciBuildQueueItem = data[0];
+      $scope.ciBuildItemQueueOrInProgress = data[0];
     });
 
     channel.bind('ci_build_update', function(data) {
@@ -49,5 +57,10 @@ angular.module('clientApp')
         }
         $scope.ciBuildItems[itemId] = data[0];
       });
+
+      // Check if the updated item is the Queue Or InProgress item.
+      if ($scope.ciBuildItemQueueOrInProgress.id == id) {
+        $scope.ciBuildItemQueueOrInProgress = data[0];
+      }
     });
   });
