1935025e70b84f667a6a41ac9b1ec83b53a9a27e
nader77 <nader@gizra.com>
2015-07-29 10:11:39 +0300

    Add breadcrumbs.

--- client/app/scripts/controllers/ci_incident.js
+++ client/app/scripts/controllers/ci_incident.js
@@ -8,9 +8,13 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('CiIncidentCtrl', function ($scope, incident) {
+  .controller('CiIncidentCtrl', function ($scope, incident, Builds) {
 
     $scope.incident = incident[0];
+
+    Builds.get($scope.incident.ci_build, 'ci_build').then(function(data) {
+      $scope.build = data[0];
+    });
 
     var incidentStart = $scope.incident.created;
     var incidentEnd = $scope.incident.updated ? $scope.incident.updated : new Date();
@@ -20,13 +24,13 @@ angular.module('clientApp')
     var hours = minutes/60;
     var days = hours/24;
 
-    if (minutes > 1) {
+    if (minutes >= 1) {
       $scope.downTime = Math.floor(minutes) + ' Minutes';
     }
-    else if (hours > 1) {
+    else if (hours >= 1) {
       $scope.downTime = Math.floor(hours) + ' Hours';
     }
-    else if (days > 1) {
+    else if (days >= 1) {
       $scope.downTime = Math.floor(days) + ' Days';
     }
   });
