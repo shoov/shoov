8b7368c1ed1f1d568bd484ec49ea90eb3ed4bfba
nader77 <nader@gizra.com>
2015-07-29 16:51:32 +0300

    Theme the CI build item page view in the client

--- client/app/scripts/controllers/ci_build_item.js
+++ client/app/scripts/controllers/ci_build_item.js
@@ -8,7 +8,12 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('CiBuildItemCtrl', function ($scope, ciBuildItem) {
+  .controller('CiBuildItemCtrl', function ($scope, ciBuildItem, Builds) {
 
     $scope.ciBuildItem = ciBuildItem[0];
+
+    // Get parent build info for the breadcrumbs.
+    Builds.get($scope.ciBuildItem.build, 'ci_build').then(function(data) {
+      $scope.build = data[0];
+    });
   });
