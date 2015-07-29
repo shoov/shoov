e52a82a46741a107ce125aaa5b2014fc4285b549
nader77 <nader@gizra.com>
2015-07-29 10:46:16 +0300

    Add info labels.

--- client/app/scripts/controllers/ci_incident.js
+++ client/app/scripts/controllers/ci_incident.js
@@ -17,7 +17,7 @@ angular.module('clientApp')
     });
 
     var incidentStart = $scope.incident.created;
-    var incidentEnd = $scope.incident.updated ? $scope.incident.updated : new Date();
+    var incidentEnd = $scope.incident.created != $scope.incident.updated ? $scope.incident.updated : (new Date().getTime())/1000;
 
     var seconds = incidentEnd-incidentStart;
     var minutes = seconds/60;
