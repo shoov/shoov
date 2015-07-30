bd41d8ec469296e21403e713f633f1883b3e2b8d
Helena Eksler <helena@gizra.com>
2015-07-30 14:23:50 +0300

    Update script.

--- shoov/modules/custom/shoov_ci_build/scripts/update_messages.php
+++ shoov/modules/custom/shoov_ci_build/scripts/update_messages.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @file
+ * Triggering the process of updating CI Build Items with the start and the
+ * end timestamps.
+ */
 
 // Get tha last message id.
 $mid = drush_get_option('mid', 0);
@@ -6,39 +11,72 @@ $mid = drush_get_option('mid', 0);
 // Get the nuber of messages to be processed.
 $batch = drush_get_option('batch', 250);
 
-$query = new EntityFieldQuery();
-$query
+// Get allowed memory limit.
+$memory_limit = drush_get_option('memory_limit', 500);
+
+$i = 0;
+
+$base_query = new EntityFieldQuery();
+$base_query
   ->entityCondition('entity_type', 'message')
   ->entityCondition('bundle', 'ci_build')
   ->propertyCondition('mid', $mid, '>')
-  ->propertyOrderBy('mid', 'DESC')
+  ->propertyOrderBy('mid', 'ASC')
   ->range(0, $batch);
 
-$query_count = clone $query;
-$count = $query_count->count();
-if ($count) {
-  drush_log(dt('@count messages were found', array('@count' => $message->mid)), 'success');
-}
-else {
+$query_count = clone $base_query;
+$count = $query_count->count()->execute();
+if (!$count) {
   drush_log('No messages were found.', 'error');
+  return;
 }
 
-$result = $query->execute();
+while ($i < $count) {
 
-$messages = message_load_multiple(array_keys($result['message']));
+// Free up memory.
+  drupal_static_reset();
+  $query = clone $base_query;
+  $result = $query->execute();
 
-foreach ($messages as $message) {
-  $wrapper = entity_metadata_wrapper('message', $message);
-  if (!$wrapper->field_ci_build_start_timestamp->value() && !$wrapper->field_ci_build_end_timestamp->value()) {
-    if (in_array($wrapper->field_ci_build_status->value(), array('in_progress', 'In progress'))) {
-      $wrapper->field_ci_build_start_timestamp->set($wrapper->field_ci_build_schedule->value());
-    }
-    elseif (in_array(strtolower($wrapper->field_ci_build_status->value()), array('error', 'done'))) {
-      $wrapper->field_ci_build_start_timestamp->set($wrapper->field_ci_build_schedule->value());
-      $wrapper->field_ci_build_end_timestamp->set($wrapper->field_ci_build_schedule->value() + 60);
-    }
-    $wrapper->save();
+  if (empty($result['message'])) {
+    return;
   }
-  
-  drush_log(dt('The last mid was @number', array('@number' => $message->mid)));
+
+  $ids = array_keys($result['message']);
+  $messages = message_load_multiple($ids);
+
+  foreach ($messages as $message) {
+    $wrapper = entity_metadata_wrapper('message', $message);
+    if (!$wrapper->field_ci_build_start_timestamp->value() && !$wrapper->field_ci_build_end_timestamp->value()) {
+      if (in_array($wrapper->field_ci_build_status->value(), array('in_progress', 'In progress'))) {
+        $wrapper->field_ci_build_start_timestamp->set($wrapper->field_ci_build_schedule->value());
+      }
+      elseif (in_array(strtolower($wrapper->field_ci_build_status->value()), array('error', 'done'))) {
+        $wrapper->field_ci_build_start_timestamp->set($wrapper->field_ci_build_schedule->value());
+        $wrapper->field_ci_build_end_timestamp->set($wrapper->field_ci_build_schedule->value() + 60);
+      }
+      $wrapper->save();
+    }
+  }
+
+  $i += 250;
+  $nid = end($ids);
+
+  $params = array(
+    '@start' => reset($ids),
+    '@end' => end($ids),
+    '@iterator' => $i,
+    '@max' => $count,
+  );
+
+  drush_print(dt('Process messages from id @start to id @end. Batch state: @iterator/@max', $params));
+
+  if (round(memory_get_usage()/1048576) >= $memory_limit) {
+    $params = array(
+      '@memory' => round(memory_get_usage()/1048576),
+      '@max_memory' => memory_get_usage(TRUE)/1048576,
+    );
+    drush_log(dt('Stopped before out of memory. Start process from the node ID @nid', array('@nid' => end($ids))), 'error');
+    return;
+  }
 }
