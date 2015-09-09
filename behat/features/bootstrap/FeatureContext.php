477d988df1f683016c1c0f3f55c69fb1f16ac9dc
Amir <amir@gizra.com>
2015-09-09 09:47:29 +0300

    Make it generic, man

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -334,7 +334,7 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
       return;
     }
 
-    $status = $this->getStatusMachineName($status);
+    $status = $this->getMachineName($status);
     $params = array('@title' => $ci_build_title);
 
     // Get the CI build.
@@ -470,11 +470,11 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
   public function theFieldValueShouldBeForTheCiBuild($field_name, $field_value, $ci_build_title) {
 
     // Rebuild the machine name.
-    $field_name = str_replace(" ", "_", "field_" . strtolower($field_name));
+    $field_name = $this->getMachineName($field_name, "field_");
 
 
     if ($field_name == "field_ci_build_status") {
-      $field_value = $this->getStatusMachineName($field_value);
+      $field_value = $this->getMachineName($field_value);
     }
 
     $params = array(
@@ -514,8 +514,8 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
    * @When I change CI build :ci_build_title status from :old_status to :new_status
    */
   public function iChangeCiBuildStatusFromTo($ci_build_title, $old_status, $new_status) {
-    $old_status = $this->getStatusMachineName($old_status);
-    $new_status = $this->getStatusMachineName($new_status);
+    $old_status = $this->getMachineName($old_status);
+    $new_status = $this->getMachineName($new_status);
 
     $params = array(
       '@title' => $ci_build_title,
@@ -546,25 +546,20 @@ class FeatureContext extends DrupalContext implements SnippetAcceptingContext {
   }
 
   /**
-   * Helper function; gets the machine name of a status from a human readable name.
+   * Helper function; gets a generic machine name of a from a human readable name.
    *
-   * @param  string $name
-   *    Human readable status name.
+   * @param string $name
+   *    Human readable name.
+   *
+   * @param string $prefix
+   *    String to prefix the machine name (e.g. "field_").
    *
    * @return string
-   *    Machine name of the status.
+   *    Generic machine name.
    */
-  private function getStatusMachineName($name) {
+  private function getMachineName($name, $prefix = "") {
 
-    $statuses = array(
-      "Queue" => "queue",
-      "In progress" => "in_progress",
-      "Done" => "done" ,
-      "Error"  =>  "error",
-    );
-
-
-    return empty($statuses[$name]) ? $name : $statuses[$name];
+    return str_replace(" ", "_", $prefix . strtolower($name));
   }
 }
 
