ae2ea69ce02a6ede479d6de040b7d8705740ab3a
Helena Eksler <helena@gizra.com>
2015-10-15 14:40:50 +0300

    Save the demo url.

--- client/app/scripts/controllers/dashboard.js
+++ client/app/scripts/controllers/dashboard.js
@@ -8,7 +8,27 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('DashboardCtrl', function ($scope, account, selectedCompany, Auth, $state, $log) {
+  .controller('DashboardCtrl', function ($scope, account, selectedCompany, Auth, $state, Account,  $log) {
+
+    /**
+     * Determine if user doesn't have any repositories monitoring yet.
+     *
+     * @type {boolean}
+     */
+    $scope.userHasNoData = !account.repository;
+
+    /**
+     * Set the entered demo url for the current user.
+     *
+     * @param url
+     *  the inserted URL.
+     */
+    $scope.saveAccountDemoUrl = function(url) {
+      var data = {};
+      data['demo_request_url'] = url;
+      Account.set(data);
+      $scope.userHasNoData = false;
+    };
 
     /**
      * Logout current user.
