adb147c2d156fd1b19aee4737670667ea88f2ee8
Amitaibu <amitai@gizra.com>
2015-11-02 12:52:45 +0200

    Add resource

---
+++ shoov/modules/custom/shoov_restful/plugins/restful/node/js_lm_incidents/1.0/ShoovJSLmIncidentsResource.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains ShoovJSLmIncidentsResource.
+ */
+
+class ShoovJSLmIncidentsResource extends \ShoovEntityBaseNode {
+
+
+  /**
+   * Overrides \RestfulEntityBaseNode::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['build'] = array(
+      'property' => 'field_js_lm_build',
+    );
+
+    $public_fields['user_id'] = array(
+      'property' => 'field_user_id',
+    );
+
+    $public_fields['errors'] = array(
+      'property' => 'field_js_lm_errors',
+    );
+
+    return $public_fields;
+  }
+}
