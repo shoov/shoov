88501c35f4bfd2e2bac83fea0e1c3b2cea99955c
Amitai Burstein <amitai@gizra.com>
2015-11-08 21:57:09 +0200

    Start composing

--- client-elm/src/elm/Dashboard.elm
+++ client-elm/src/elm/Dashboard.elm
@@ -1,7 +1,7 @@
 module Dashboard where
 
 import Effects exposing (Effects)
-import Html exposing (a, div, li, i, span, text, ul, Html)
+import Html exposing (a, div, h2, hr, li, i, span, text, ul, Html)
 import Html.Attributes exposing (id, class)
 import RouteHash exposing (HashUpdate)
 
@@ -42,9 +42,9 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div
-    [ id "homepage" ]
-    [ div
+  let
+    header =
+      div
         [ class "row"]
         [ div
             [ class "col-sm-12" ]
@@ -58,12 +58,63 @@ view address model =
                         , span [] [ text "Dashboard" ]
                         ]
                     ]
-
+                , hr [ class "no-margin" ] []
                 ]
             ]
         ]
 
-    ]
+    mainContent =
+      div
+        [ class "main-content" ]
+        [ div
+            [ class "wrapper"]
+            [ h2 [] [ text "Dashboard" ]
+            , dashboardLinks
+            ]
+        ]
+
+    dashboardLinks =
+      div
+      [ id "dashboard-links"]
+      [ div
+          [ class "row text-center"]
+          []
+      ]
+    -- <div class "main-content">
+    --   <div class="wrapper">
+    --     <h2>Dashboard</h2>
+    --     <div id="dashboard-links">
+    --       <div class="row text-center">
+    --         <div class="col-sm-6 link">
+    --           <div>
+    --             <a ui-sref="dashboard.repos">
+    --               <div class="main-icon">
+    --                 <i class="fa fa-heartbeat"></i>
+    --               </div>
+    --               <h5>Live Monitor</h5>
+    --             </a>
+    --           </div>
+    --         </div>
+    --         <div class="col-sm-6 link">
+    --           <div>
+    --             <a ui-sref="dashboard.builds">
+    --               <div class="main-icon">
+    --                 <i class="fa fa-history"></i>
+    --               </div>
+    --               <h5>Visual Regression</h5>
+    --             </a>
+    --           </div>
+    --         </div>
+    --       </div>
+    --     </div>
+    --   </div>
+
+  in
+    div
+      [ id "homepage" ]
+      [ header
+      , mainContent
+      ]
 
 -- ROUTER
 
