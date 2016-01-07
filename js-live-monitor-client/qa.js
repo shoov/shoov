4e2530af09e5783e7b5abcc41ecb9094ec34b685
Helena Eksler <helena@gizra.com>
2016-01-07 17:39:30 +0200

    Change content-type header.

--- js-live-monitor-client/qa.js
+++ js-live-monitor-client/qa.js
@@ -11,77 +11,68 @@ runTests = function() {
   var errors = [];
   var buildId = 17;
   var buildToken = '45974c049b94a808ede67034c3ad2dec';
-  var result = [
-    {
-      id: "it should find the a visible add to cart link",
-      result: function() {
-        var element = document.querySelector('a.cart');
-        return !!element && element.offsetParent !== null;
+
+  loadJS('http://localhost/shoov/www/js_lm/' + buildId, function () {
+    result = customTests();
+    console.log(result);
+
+    result.forEach(function (row) {
+      if (!!row.result()) {
+        return;
       }
-    },
-    {
-      id: "it should find a visible title",
-      result: function() {
-        var element = document.querySelector('h1');
-        return !!element && element.offsetParent !== null;
-      }
-    }
-  ];
-  result.forEach(function(row) {
-    if (!!row.result()) {
-      return;
-    }
 
-    errors.push(row.id);
-  });
+      errors.push(row.id);
+    });
 
-  var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-  var data = {
-    build: buildId,
-    url: '/',
-    errors: errors.join("\r\n")
-  };
+    var data = {
+      build: buildId,
+      url: '/',
+      errors: errors.join("\r\n")
+    };
 
-  var serializeObject = function(obj) {
+    var serializeObject = function (obj) {
       var pairs = [];
       for (var prop in obj) {
-          if (!obj.hasOwnProperty(prop)) {
-              continue;
-          }
-          pairs.push(prop + '=' + obj[prop]);
+        if (!obj.hasOwnProperty(prop)) {
+          continue;
+        }
+        pairs.push(prop + '=' + obj[prop]);
       }
       return pairs.join('&');
-  }
+    }
 
 
-  html2canvas(document.body, {
-    onrendered: function(canvas) {
-      data.image = canvas.toDataURL("image/png");
+    html2canvas(document.body, {
+      onrendered: function (canvas) {
+        data.image = canvas.toDataURL("image/png");
 
-      var image = document.createElement('img');
-      image.src = canvas.toDataURL("image/png");
-      document.body.appendChild(image);
+        var image = document.createElement('img');
+        image.src = canvas.toDataURL("image/png");
+        document.body.appendChild(image);
 
-      console.log(image.src);
+        console.log(image.src);
 
 
-      request.open('POST', 'http://localhost/shoov/www/api/v1.0/js-lm-incidents?token=' + buildToken + '&XDEBUG_SESSION_START=14229', true);
-      request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.open('POST', 'http://localhost/shoov/www/api/v1.0/js-lm-incidents?token=' + buildToken + '&XDEBUG_SESSION_START=14229', true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
-      request.onload = function() {
+        request.onload = function () {
           if (request.status === 200) {
-              console.log('ok');
+            console.log('ok');
           }
           else if (request.status !== 200) {
-              console.log('Request failed.  Returned status of ' + request.status);
+            console.log('Request failed.  Returned status of ' + request.status);
           }
-      };
+        };
 
-      request.send(encodeURI(serializeObject(data)));
-    }
+        request.send(encodeURI(serializeObject(data)));
+      }
+    });
   });
 }
+
 
 function loadJS(src, callback) {
     var s = document.createElement('script');
